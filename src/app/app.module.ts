afb4a8ea1b273207fb8d12d646818255bb6bd012
Hantke <bruno.hantke@gmail.com>
2022-06-14 01:34:57 -0300

    gitflow-feature-stash: feature-atividade-05

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MusicDetailComponent } from './music-detail/music-detail.component';
+import { CadastroFormComponent } from './cadastro-form/cadastro-form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MusicDetailComponent,
+    CadastroFormComponent
   ],
   imports: [
     BrowserModule,
