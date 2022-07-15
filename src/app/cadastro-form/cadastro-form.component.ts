7780396b6c6c1ab65e1609300c26878605840cb8
Hantke <bruno.hantke@gmail.com>
2022-07-15 03:48:25 -0300

    gitflow-feature-stash: feature-atividade-09

--- src/app/cadastro-form/cadastro-form.component.ts
+++ src/app/cadastro-form/cadastro-form.component.ts
@@ -1,23 +1,49 @@
+import { MusicaServiceService } from './../service/musica-service.service';
+import { Musica } from './../model/musica';
 import { Component, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro-form',
   templateUrl: './cadastro-form.component.html',
-  styleUrls: ['./cadastro-form.component.css']
+  styleUrls: ['./cadastro-form.component.css'],
+  providers:[MusicaServiceService]
 })
 export class CadastroFormComponent implements OnInit {
+
+  musicas?:Musica[];
+
+  formMusica!: FormGroup;
 
   @Input() nome!:String;
   dificuldades = ['Fácil', 'Médio', 'Difícil'];
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private musicasService:MusicaServiceService) { }
 
   ngOnInit(): void {
+    this.criarFormulario(new Musica());
   }
 
 onSubmit(){
+
+  if(!this.formMusica.valid){
+    return;
+  }
+
+  this.musicasService.save(this.formMusica.value);
+
   alert('Teste');
-  console.log('TESTE')
+}
+
+criarFormulario(musica: Musica) {
+  this.formMusica = this.formBuilder.group({
+    id: null,
+    nomeMusica: [musica.nomeMusica],
+    nomeBanda: [musica.nomeBanda],
+    dificuldades:[musica.dificuldade],
+    tempo:[musica.tempo],
+    urlImagem:[musica.urlImagem]
+  });
 }
 
 }
